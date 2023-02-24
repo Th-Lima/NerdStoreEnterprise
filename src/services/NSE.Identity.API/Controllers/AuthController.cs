b7cc0031937dae0d391877464941e2397c808f68
Thales Lima <lthales53@gmail.com>
2023-02-23 22:05:56 -0300

    Implementing customized response

--- src/services/NSE.Identity.API/Controllers/AuthController.cs
+++ src/services/NSE.Identity.API/Controllers/AuthController.cs
@@ -1,23 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-using NSE.Identity.API.Extensions;
 using NSE.Identity.API.Models;
 using NSE.Identity.API.Services.Interfaces;
-using System;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
 using System.Threading.Tasks;
-using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 
 namespace NSE.Identity.API.Controllers
 {
-    [ApiController]
     [Route("api/identidade")]
-    public class AuthController : Controller
+    public class AuthController : MainController
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
@@ -33,7 +23,7 @@ namespace NSE.Identity.API.Controllers
         public async Task<ActionResult> Register(UserRegister userRegister)
         {
             if (!ModelState.IsValid)
-                return BadRequest();
+                return CustomResponse(ModelState);
 
             var user = new IdentityUser
             {
@@ -45,81 +35,38 @@ namespace NSE.Identity.API.Controllers
             var result = await _userManager.CreateAsync(user, userRegister.Password);
             if (result.Succeeded)
             {
-                await _signInManager.SignInAsync(user, isPersistent: false);
-
-                return Ok(await _jwtService.GenerateJwtAsync(userRegister.Email));
+                return CustomResponse(await _jwtService.GenerateJwtAsync(userRegister.Email));
             }
 
-            return BadRequest();
+            foreach (var resultError in result.Errors)
+            {
+                AddErrorsProcessing(resultError.Description);
+            }
+
+            return CustomResponse();
         }
 
         [HttpPost("autenticar")]
         public async Task<ActionResult> Login(UserLogin userLogin)
         {
             if (!ModelState.IsValid)
-                return BadRequest();
+                return CustomResponse(ModelState);
 
             var result = await _signInManager.PasswordSignInAsync(userLogin.Email, userLogin.Password, isPersistent: false, lockoutOnFailure: true);
 
             if (result.Succeeded)
-            {
-                return Ok(await _jwtService.GenerateJwtAsync(userLogin.Email));
+                return CustomResponse(await _jwtService.GenerateJwtAsync(userLogin.Email));
 
+            if (result.IsLockedOut)
+            {
+                AddErrorsProcessing("Usuário temporariamente bloqueado por tentativas inválidas");
+
+                return CustomResponse();
             }
 
-            return BadRequest();
+            AddErrorsProcessing("Usuário ou Senha incorretos");
+
+            return CustomResponse();
         }
-
-        //private async Task<UserResponseLogin> GenerateJwt(string email)
-        //{
-        //    var user = await _userManager.FindByNameAsync(email);
-        //    var claims = await _userManager.GetClaimsAsync(user);
-        //    var userRoles = await _userManager.GetRolesAsync(user);
-
-        //    claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
-        //    claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
-        //    claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
-        //    claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
-        //    claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));
-
-        //    foreach (var userRole in userRoles)
-        //    {
-        //        claims.Add(new Claim("role", userRole));
-        //    }
-
-        //    var identityClaims = new ClaimsIdentity();
-        //    identityClaims.AddClaims(claims);
-
-        //    var tokenHandler = new JwtSecurityTokenHandler();
-        //    var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
-
-        //    var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
-        //    {
-        //        Issuer = _appSettings.Issuer,
-        //        Audience = _appSettings.ValidatedAt,
-        //        Subject = identityClaims,
-        //        Expires = DateTime.Now.AddHours(_appSettings.ExpirationHours),
-        //        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-        //    });
-
-        //    var encodedToken = tokenHandler.WriteToken(token);
-
-        //    var respnse = new UserResponseLogin()
-        //    {
-        //        AccessToken = encodedToken,
-        //        ExpiresIn = TimeSpan.FromHours(_appSettings.ExpirationHours).TotalSeconds,
-        //        UserToken = new UserToken
-        //        {
-        //            Id = user.Id,
-        //            Email = user.Email,
-        //            Claims = claims.Select(x => new UserClaim { Type = x.Type, Value = x.Value })
-        //        }
-        //    };
-
-        //    return respnse;
-        //}
-
-        //private static long ToUnixEpochDate(DateTime date) =>
-        //    (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
     }
 }
---
+++ src/services/NSE.Identity.API/Controllers/MainController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore.Internal;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NSE.Identity.API.Controllers
+{
+    [ApiController]
+    public abstract class MainController : Controller
+    {
+        protected ICollection<string> Erros = new List<string>();
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if(ValidOperation())
+                return Ok(result);
+
+            return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]>
+            {
+                { "Mensagens", Erros.ToArray() }
+            }));
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            var errors = modelState.Values.SelectMany(x => x.Errors);
+            foreach (var error in errors)
+            {
+                AddErrorsProcessing(error.ErrorMessage);
+            }
+
+            return CustomResponse();
+        }
+
+        protected bool ValidOperation()
+        {
+            return !Erros.Any();
+        }
+
+        protected void AddErrorsProcessing(string error)
+        {
+            Erros.Add(error);
+        }
+
+        protected void ClearErrorsProcessing()
+        {
+            Erros.Clear();
+        }
+    }
+}
