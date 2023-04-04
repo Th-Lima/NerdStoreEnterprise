8b78c2b23b8788605b4f96cc2fc4c94aedf68422
Thales Lima <lthales53@gmail.com>
2023-04-03 23:01:42 -0300

    Defining entities

---
+++ src/services/NSE.Client.API/Models/Address.cs
@@ -0,0 +1,38 @@
+﻿using NSE.Core.DomainObjects;
+using System;
+
+namespace NSE.Client.API.Models
+{
+    public class Address : Entity
+    {
+        public string AddressPlace { get; private set; }
+
+        public string NumberAddress { get; private set; }
+
+        public string Complement { get; private set; }
+
+        public string ZipCode { get; private set; }
+
+        public string Neighborhood { get; private set; }
+
+        public string City { get; private set; }
+
+        public string State { get; private set; }
+
+        public Guid ClientId { get; private set; }
+
+        //EF Relation
+        public Client Client { get; protected set; }
+
+        public Address(string addressPlace, string numberAddress, string complement, string zipCode, string neighborhood, string city, string state)
+        {
+            AddressPlace = addressPlace;
+            NumberAddress = numberAddress;
+            Complement = complement;
+            ZipCode = zipCode;
+            Neighborhood = neighborhood;
+            City = city;
+            State = state;
+        }
+    }
+}
---
+++ src/services/NSE.Client.API/Models/Client.cs
@@ -0,0 +1,24 @@
+﻿using NSE.Core.DomainObjects;
+
+namespace NSE.Client.API.Models
+{
+    public class Client : Entity, IAggregateRoot
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string CPF { get; private set; }
+        public bool IsExcluded { get; set; }
+        public Address Address { get; private set; }
+
+        //EF Relation
+        protected Client(){}
+
+        public Client(string name, string email, string cpf)
+        {
+            Name = name;
+            Email = email;
+            CPF = cpf;
+            IsExcluded = false;
+        }
+    }
+}
