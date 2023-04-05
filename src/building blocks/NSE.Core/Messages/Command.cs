5b6e63c85d69167da74ac29d9317a17c1554d263
Thales Lima <lthales53@gmail.com>
2023-04-04 22:27:07 -0300

    Initializing implementation of command and command handler

---
+++ src/building blocks/NSE.Core/Messages/Command.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Results;
+using System;
+
+namespace NSE.Core.Messages
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public virtual bool IsValid()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/services/NSE.Client.API/Application/Commands/CustomerCommandHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace NSE.Client.API.Application.Commands
+{
+    public class CustomerCommandHandler
+    {
+        public void Handle(RegisterCustomerCommand message)
+        {
+
+        }
+    }
+}
