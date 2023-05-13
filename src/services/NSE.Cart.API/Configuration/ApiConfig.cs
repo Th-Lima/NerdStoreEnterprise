0f385c08104c681180f15100d4aa5a76a2614808
Thales Lima <lthales53@gmail.com>
2023-05-13 16:21:34 -0300

    Implementing Grpc server pt II

--- src/services/NSE.Cart.API/Configuration/ApiConfig.cs
+++ src/services/NSE.Cart.API/Configuration/ApiConfig.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NSE.Cart.API.Data;
+using NSE.Cart.API.Services.gRPC;
 using NSE.WebAPI.Core.Identity;
 
 namespace NSE.Cart.API.Configuration
@@ -17,6 +18,8 @@ namespace NSE.Cart.API.Configuration
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllers();
+
+            services.AddGrpc();
 
             services.AddCors(options =>
             {
@@ -47,6 +50,7 @@ namespace NSE.Cart.API.Configuration
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapGrpcService<CartGrpcService>().RequireCors("Total");
             });
         }
     }
---
+++ src/services/NSE.Cart.API/Services/gRPC/CartGrpcService.cs
@@ -0,0 +1,84 @@
+﻿using Grpc.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NSE.Cart.API.Data;
+using NSE.Cart.API.Models;
+using NSE.WebAPI.Core.User;
+using System.Threading.Tasks;
+
+namespace NSE.Cart.API.Services.gRPC
+{
+    [Authorize]
+    public class CartGrpcService : CartShopping.CartShoppingBase
+    {
+        private readonly ILogger<CartGrpcService> _logger;
+
+        private readonly IAspNetUser _user;
+        private readonly CartContext _context;
+
+        public CartGrpcService(
+            ILogger<CartGrpcService> logger,
+            IAspNetUser user,
+            CartContext context)
+        {
+            _logger = logger;
+            _user = user;
+            _context = context;
+        }
+
+        public override async Task<CartCustomerResponse> GetCart(GetCartRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation("Chamando GetCart");
+
+            var cart = await GetCartCustomer() ?? new CartCustomer();
+
+            return MapCartCustomerToProtoResponse(cart);
+        }
+
+        private async Task<CartCustomer> GetCartCustomer()
+        {
+            return await _context.CartCustomers
+                .Include(c => c.Itens)
+                .FirstOrDefaultAsync(c => c.CustomerId == _user.GetUserId());
+        }
+
+        private static CartCustomerResponse MapCartCustomerToProtoResponse(CartCustomer cart)
+        {
+            var cartProto = new CartCustomerResponse
+            {
+                Id = cart.Id.ToString(),
+                Customerid = cart.CustomerId.ToString(),
+                Totalvalue = (double)cart.TotalValue,
+                Discount = (double)cart.Discount,
+                Voucherused = cart.VoucherUsed,
+            };
+
+            if (cart.Voucher != null)
+            {
+                cartProto.Voucher = new VoucherResponse
+                {
+                    Code = cart.Voucher.Code,
+                    Percentage = (double?)cart.Voucher.Percentage ?? 0,
+                    Valuediscount = (double?)cart.Voucher.ValueDiscount ?? 0,
+                    Typediscount = (int)cart.Voucher.TypeDiscount
+                };
+            }
+
+            foreach (var item in cart.Itens)
+            {
+                cartProto.Itens.Add(new CartItemResponse
+                {
+                    Id = item.Id.ToString(),
+                    Name = item.Name,
+                    Image = item.Image,
+                    Productid = item.ProductId.ToString(),
+                    Amount = item.Amount,
+                    Price = (double)item.Price
+                });
+            }
+
+            return cartProto;
+        }
+    }
+}
